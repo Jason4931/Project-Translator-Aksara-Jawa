43fc2dfa47061db7cb694dc8054391e24c0c793f
Jason4931 <emmanuel.jason2008@gmail.com>
2024-02-26 07:40:26 +0700

    py server

--- audio/main.py
+++ audio/main.py
@@ -1,10 +1,14 @@
 import whisper
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
 @app.get('/audio')
 def transcribe(audio):
     model = whisper.load_model("tiny")
     result = model.transcribe(audio)
-    # print(result["text"])
     return result["text"]
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
